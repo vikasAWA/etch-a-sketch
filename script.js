a8e70aff7ac06548fbc29e17bb367887f0162ac4
vikasAWA <85212839+vikasAWA@users.noreply.github.com>
2024-07-02 16:10:48 +0530

    Change color feature added
    This feature will change the color of square when hovering over it .

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const container = document.querySelector('.container');
 
-
+//function to fill container with the squares
 function makeSquaresGrid(squaresPerSide) {
     for (let i = 0; i < squaresPerSide*squaresPerSide; i++) {
         let div = document.createElement('div');
@@ -10,4 +10,15 @@ function makeSquaresGrid(squaresPerSide) {
 }
 
 makeSquaresGrid(16);
+
+
+// Function to change color on an event
+function changeColor(event) {
+    event.target.style.backgroundColor = "#414141";
+}
+
+//Added an mouseover event listener to each square. Square will change it's color whenever we hover over it.
+Array.from(container.children).forEach((child) => {
+    child.addEventListener("mouseover", changeColor)
+})
 
