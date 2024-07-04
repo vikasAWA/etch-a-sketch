e3e74f3e1b4b314cdac8531ae1bdba7a34b3488f
vikasAWA <85212839+vikasAWA@users.noreply.github.com>
2024-07-04 14:38:24 +0530

    Added rainbow color feature
    Added a function to get different random colors on tiles/square.

--- script.js
+++ script.js
@@ -11,10 +11,17 @@ function makeSquaresGrid(squaresPerSide) {
 
 makeSquaresGrid(16);
 
+//Function to get rainbow effect on squares.
+function rainbow() {
+    return `rgb(${Math.floor(Math.random() * 255)} ${Math.floor(Math.random() * 255)} ${Math.floor(Math.random() * 255)} / ${Math.random() * 0.999
+
+    })`;
+}
+
 
 // Function to change color on an event
 function changeColor(event) {
-    event.target.style.backgroundColor = "#414141";
+    event.target.style.backgroundColor = rainbow();
 }
 
 //Added an mouseover event listener to each square. Square will change it's color whenever we hover over it.
